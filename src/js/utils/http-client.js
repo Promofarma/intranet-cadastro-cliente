1b2b1133246a5538586f0191a19dc1ae02664b16
Vinicius Coutinho <vinicius@promofarma.com.br>
2025-05-28 09:16:45 -0300

    :feat Change API URL

--- src/js/utils/http-client.js
+++ src/js/utils/http-client.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://promoclube.promofarma.com.br/api/v1";
+const BASE_URL = "http://192.168.1.11:8184/api/v1";
 
 export const httpClient = async (uri, options) => {
   return await fetch(BASE_URL + uri, {
